492c80f7a9c8f9f3bdcc30ec7055d89b02ca9a4d
Deepali Tomar <143430573+deepalitomar021@users.noreply.github.com>
2024-03-10 16:42:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ google = oauth.register(
 
 @app.route('/login')
 def login():
-    # redirect_uri = url_for('history', _external=True)
-    redirect_uri='https://news-analysis-oowi.onrender.com/history',
+    redirect_uri = url_for('history', _external=True)
+    
     return google.authorize_redirect(redirect_uri)
 
 @app.route('/authorize')
